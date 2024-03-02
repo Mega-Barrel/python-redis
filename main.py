d75c6b42bfc3d1b87530d61fd221a271b0d463fb
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-03-02 20:19:51 +0530

    added redis keys

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ if __name__ == "__main__":
         host='localhost',
         port=6379
     )
-    r.set("United Kingdom", "London")
-    r.set("Germany", "Berlin")
-    r.set("India", "New Delhi")
+    redis_cli = redis.Redis(
+        host='localhost',
+        port=6379,
+        charset="utf-8",
+        decode_responses=True
+    )
+
+    #Set your key
+    redis_cli.set("United Kingdom", "London")
+    redis_cli.set("Germany", "Berlin")
+    redis_cli.set("India", "New Delhi")
+
+    #Get the value of inserted key
+    print(redis_cli.get('United Kingdom'))
+    print(redis_cli.get('Germany'))
+    print(redis_cli.get('India'))
